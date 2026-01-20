eb3bcfb975bbb1786ab63a384ec76ae5638e8605
joewatech <joewatech@outlook.com>
2026-01-19 22:58:56 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame, sys
+from spaceship import Spaceship
+
+pygame.init()
+
+SCREEN_WIDTH = 750
+SCREEN_HEIGHT = 700
+
+GREY = (29, 29, 27)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Space Invaders Python")
+
+clock = pygame.time.Clock()
+
+spaceship = Spaceship()
+spaceship_group = pygame.sprite.GroupSingle()
+spaceship_group.add(spaceship)
+
+while True:
+    # Checking for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    # Drawing
+    screen.fill(GREY)
+    spaceship_group.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
